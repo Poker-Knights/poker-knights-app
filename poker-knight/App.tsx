8f2bc5dad095df41d47a896a58d902b1e24bab5c
Matthew Morello <118625106+mmorello4@users.noreply.github.com>
2024-01-27 19:41:14 -0500

    making changes for fun p2 (#8)
    * Changes to text, line by line, idk having fun
    
    * Changes to text, line by line, idk having fun p2

--- poker-knight/App.tsx
+++ poker-knight/App.tsx
@@ -4,12 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{'-> Insert Poker Knight app here!'}</Text>
+      <Text>-----------------------------------</Text>
+      <Text>{'-> Insert Poker Knight app here! <-'}</Text>
       <Text>David was here!</Text>
       <Text>Sri was here!</Text>
       <Text>Matthew was here!</Text>
-      <Text>--------------------</Text>
-
+      <Text>-----------------------------------</Text>
       <StatusBar style="auto" />
     </View>
   );
