c7e28e9a96d109dd78d615551780842c3f45f88c
Matthew Morello <118625106+mmorello4@users.noreply.github.com>
2024-01-27 19:36:04 -0500

    Changes to text, line by line, idk having fun (#7)

--- poker-knight/App.tsx
+++ poker-knight/App.tsx
@@ -4,8 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{'-> Insert Poker Knight app here! Sri was here! Matthew was here! <-\n\n'}</Text>
+      <Text>{'-> Insert Poker Knight app here!'}</Text>
       <Text>David was here!</Text>
+      <Text>Sri was here!</Text>
+      <Text>Matthew was here!</Text>
+      <Text>--------------------</Text>
 
       <StatusBar style="auto" />
     </View>
