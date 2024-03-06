6392bf299003f2edba32a6de01a67ef48102bc6e
Matthew Morello <mattiemore7@gmail.com>
2024-03-06 11:57:20 -0500

    Studf

--- poker-knight/server/join_screen/handleAttemptToJoin.tsx
+++ poker-knight/server/join_screen/handleAttemptToJoin.tsx
@@ -22,6 +22,7 @@ export const handleAttemptToJoin =
       createAndAddPlayer(username, socket.id, game);
 
       socket.emit("gameJoined", { gameState: game });
+      console.log("Player Joined");
     } else {
       socket.emit("gameNotFound", { gameID: inputGameID });
     }
