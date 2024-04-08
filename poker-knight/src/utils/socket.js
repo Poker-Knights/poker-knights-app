bafe9a5aa55a1b94960e9cc5252d98e13eb6dd02
Matthew Morello <mattiemore7@gmail.com>
2024-04-08 10:33:20 -0400

    Update socket.js

--- poker-knight/src/utils/socket.js
+++ poker-knight/src/utils/socket.js
@@ -1,4 +1,4 @@
-const SERVER_URL = "http://172.20.10.13:3000/";
+const SERVER_URL = "http://172.20.10.12:3000/";
 
 const PLAYER_COUNT = 4; // sync this with number of players we want to allow to loading screen
 
