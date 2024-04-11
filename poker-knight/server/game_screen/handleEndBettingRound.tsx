a51c6852221069aede50ae9dca9359f977f30de8
SrinilaiK <105080244+SrinilaiK@users.noreply.github.com>
2024-04-11 00:42:20 -0400

    Revert "Betting logic"

--- poker-knight/server/game_screen/handleEndBettingRound.tsx
+++ poker-knight/server/game_screen/handleEndBettingRound.tsx
@@ -1,39 +1,21 @@
 import { Socket } from "socket.io";
 import { Game } from "../../src/types/Game";
 
-export const handleEndBettingRound = (game: Game) => {
-  // Increment Betting Round
-  game.curBettingRound++;
+export const handleStartBettingRound =
+  (Socket: Socket, games: { [key: string]: Game }) => (inputGameID: string) => {
+    // Find the game with the given ID
+    const game = games[inputGameID];
 
-  // Save current pots
-  game.players.forEach((curPlayer) => {
-    // If the player had to go all in
-    if (curPlayer.allInFg) {
-      let valToSub = 0; // Value to store the sum of the bets not matched by player splitting pot
-      // Loop through each player, skip the current player and players folded
-      game.players.forEach((player2) => {
-        if (player2.name === curPlayer.name || player2.foldFG) return;
-        // If the player were looking at has bet more then the current player, subtract the difference
-        if (curPlayer.lastBet < player2.lastBet) {
-          valToSub += player2.lastBet - curPlayer.lastBet;
-        }
-      });
-      curPlayer.splitPotVal = game.potSize - valToSub; // Subtract out value not matched to achieve proper pot size
-    } else if (!curPlayer.foldFG) {
-      curPlayer.splitPotVal = game.potSize; // If not all in and in, split pot is total pot
-    }
-  });
+    // Increment Betting Round
+    game.curBettingRound++;
 
-  /*
-  current pot: 500 = lastFullRound + joshCur + kevCur + mattCur + sriCur -> 500 - 80 - 80 - 56 - 70 = 214
-          Josh (100)      Kevin (100)       Matthew (56)       Sri (70)
-last bet:    80               80                56                70
-splitpotFG:   0                0                 1                 1
-  |
-  |--->     currentpot - (foreach.players.lastbet > curplayer.lastbet)
-          ex: Sri -> 500 - (80-70) - (80 -70) = 480
-          ex: Matt -> 500 - (80-56) - (80-56) - (70-56) = 438
-  */
+    // Save current pots
+    game.players.forEach((player) => {
+      player.splitPotVal = game.potSize;
+    });
 
-  return game;
-};
+    // emit the updated game
+    Socket.emit("startBettingRound", game);
+
+    return;
+  };
--- poker-knight/server/game_screen/handleStartBettingRound.tsx
+++ poker-knight/server/game_screen/handleStartBettingRound.tsx
@@ -1,52 +1,59 @@
 import { Socket } from "socket.io";
 import { Game } from "../../src/types/Game";
 
-export const handleStartBettingRound = (game: Game) => {
-  // Assign the first player as little blind and second player as big blind
-  let players = game.players;
+export const handleStartBettingRound =
+  (Socket: Socket, games: { [key: string]: Game }) => (inputGameID: string) => {
+    // Find the game with the given ID
+    const game = games[inputGameID];
 
-  // Assign Game Updates
-  game.currentBet = 0;
+    // Assign the first player as little blind and second player as big blind
+    let players = game.players;
 
-  // every players current bet is -1
-  players.forEach((player) => {
-    player.lastBet = 0;
-    player.currentTurn = false;
-  });
+    // Assign Game Updates
+    game.currentBet = 0;
 
-  // make the player after the big blind the current player
-  let curPlayerInd = game.curBigBlind + 1;
-  while (game.players[curPlayerInd - 1].foldFG) {
-    curPlayerInd = (curPlayerInd + 1) % (game.playerCount + 1);
-    if (curPlayerInd === 0) curPlayerInd = 1;
-  }
-  game.currentPlayer = curPlayerInd; // Set new player index
-  players[game.currentPlayer - 1].currentTurn = true;
+    // every players current bet is -1
+    players.forEach((player) => {
+      player.lastBet = -1;
+      player.currentTurn = false;
+    });
 
-  // set players equal to game players
-  game.players = players;
+    // make the player after the big blind the current player
+    let curPlayerInd = game.curBigBlind + 1;
+    while (game.players[curPlayerInd - 1].foldFG) {
+      curPlayerInd = (curPlayerInd + 1) % 5;
+      if (curPlayerInd === 0) curPlayerInd = 1;
+    }
+    game.currentPlayer = curPlayerInd; // Set new player index
+    players[game.currentPlayer - 1].currentTurn = true;
 
-  // call function to deal cards here
-  switch (game.curBettingRound) {
-    case 0:
-      // No cards dealt
-      break;
-    case 1:
-      // Deal 3 cards
-      // Burn 1 card
-      break;
-    case 2:
-      // Deal 1 Card
-      // Burn 1 card
-      break;
-    case 3:
-      // Deal 1 Card
-      // Burn 1 card
-      break;
-    default:
-      // nothing
-      break;
-  }
+    // set players equal to game players
+    game.players = players;
 
-  return game;
-};
+    // call function to deal cards here
+    switch (game.curBettingRound) {
+      case 0:
+        // No cards dealt
+        break;
+      case 1:
+        // Deal 3 cards
+        // Burn 1 card
+        break;
+      case 2:
+        // Deal 1 Card
+        // Burn 1 card
+        break;
+      case 3:
+        // Deal 1 Card
+        // Burn 1 card
+        break;
+      default:
+        // nothing
+        break;
+    }
+
+    // emit the updated game
+    Socket.emit("bettingRoundStarted", game);
+
+    return;
+  };
