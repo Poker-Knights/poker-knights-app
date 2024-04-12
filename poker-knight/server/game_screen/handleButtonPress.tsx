976387c3ff181665446bd951a4e4fc9922ba9a1d
DarkShadowNova <sri567890@gmail.com>
2024-04-12 02:42:29 -0400

    Potentially fixed the infinite checking issue as well as a possible while loop issue in handleButtonPress

--- poker-knight/server/game_screen/handleButtonPress.tsx
+++ poker-knight/server/game_screen/handleButtonPress.tsx
@@ -78,16 +78,27 @@ export const handleButtonPress = (
 // Function to handle a player's turn, return player
 export const nextPlayer = (game: Game) => {
   // Logic to handle player's turn
-  game.players[game.currentPlayer - 1].currentTurn = false; // Set current player turn to true
+
+  // Ctr variable should be re-defined for each time nextPlayer() is called,
+  // therefore, there should never be a wrong break in the while loop.
+  let ctr = 0;
+  game.players[game.currentPlayer - 1].currentTurn = false; // Set previous current player turn to false
 
   let curPlayer = game.currentPlayer + 1;
   if (curPlayer === 5) curPlayer = 1;
   while (game.players[curPlayer - 1].foldFG) {
     curPlayer = (curPlayer + 1) % (game.playerCount + 1);
     if (curPlayer === 0) curPlayer = 1;
+    ctr++;
+
+    // This is to make sure we are not stuck in an infinite while loop, since
+    // if all players have foldFG set to true, the while loop will never break.
+    // (If this isn't a potential issue, remove this if statement and the ctr variable's declaration.)
+    if (ctr === game.playerCount)
+      break;
   }
   game.currentPlayer = curPlayer;
-  game.players[game.currentPlayer - 1].currentTurn = true; // Set current player turn to true
+  game.players[game.currentPlayer - 1].currentTurn = true; // Set new current player turn to true
 
   return game;
 };
